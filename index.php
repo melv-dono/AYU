b43123e54b63bf37ef3f69d7f4c92f48c2c0df2d
T1masv <timotei.gerante@gmail.com>
2021-12-15 18:51:41 +0100

    ADD:bootstrap

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>AYU</title>
+</head>
+<body>
 <?php
-require_once("./db.php");
-require_once("./module/mod_connexion/mod_connexion.php");
+    require_once("./db.php");
+    require_once("./module/mod_connexion/mod_connexion.php");
 
-$modConnexion = new Mod_connexion();
-$modConnexion->display();
+    $modConnexion = new Mod_connexion();
+    $modConnexion->display();    
+?>
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</body>
--- module/mod_connexion/vue_connexion.php
+++ module/mod_connexion/vue_connexion.php
@@ -5,19 +5,22 @@ Class Vue_connexion{
   }
   function affiche_form(){
     echo '
-      <body>
-        <link rel="stylesheet" href="./module/mod_connexion/styles/style.css">
-        <main>
-          <h2>Log in</h2>
-          <form class="login_form" action="index.php?action=" method="post">
-            <label for="login_input">Login</label>
-            <input type="text" name="email_input" required value="">
-            <label for="password_input">Password</label>
-            <input type="password" name="password_input" required value="">
-            <button class="submit">lesss goooo</button>
-          </form>
-        </main>
-      </body>
+      
+      <link rel="stylesheet" href="./module/mod_connexion/styles/style.css">
+      <main>
+        <h2>Login</h2>
+        <form>
+          <div class="mb-3">
+            <label for="username_input" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username_input">
+          </div>
+          <div class="mb-3">
+            <label for="password_input" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password_input">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </main>
     ';
   }
 }
