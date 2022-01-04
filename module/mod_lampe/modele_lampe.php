d747721e995a1e33e56032f3d022973b8dce9aad
Minh-Triii <minhtrimn.ng@gmail.com>
2022-01-04 20:10:14 +0100

    Avancement_Lampe

--- module/mod_lampe/modele_lampe.php
+++ module/mod_lampe/modele_lampe.php
@@ -1,7 +1,18 @@
 <?php
+    require_once('./db.php');
     class Modele_lampe {
         public function __construct() {
             
         }
+
+        public function getListeLampesSalle($numSalle){
+            $idTypeLampe=1
+            $selectRequete = self::$bdd->prepare("SELECT * FROM equipement where idType = ? and numerosalle = ?;");
+            $selectRequete->execute(array($idTypeLampe, $numSalle));
+            $result=$selectRequete->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+
+
     }
 ?>
--- module/mod_lampe/vue_lampe.php
+++ module/mod_lampe/vue_lampe.php
@@ -4,7 +4,7 @@
             
         }
 
-        public function luminositer() {
+        public function luminosite() {
             
         }
 
