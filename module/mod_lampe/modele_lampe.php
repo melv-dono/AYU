cf2d51f65ff6e660940916c5f04dbab98cca41a6
Minh-Triii <minhtrimn.ng@gmail.com>
2022-01-24 20:54:36 +0100

    correction modele lampe et chauffage

--- module/mod_lampe/modele_lampe.php
+++ module/mod_lampe/modele_lampe.php
@@ -5,43 +5,53 @@
 
         }
 
-        //public function allumer($numSalle, $allumer){
         public function allumer($allumer){
-            if($allumer == 1){
-                $selectRequete = parent::$db->prepare("UPDATE equipement SET etat=?;");
-                //$selectRequete = parent::$db->prepare("UPDATE equipement SET etat=? WHERE numerosalle=?;");
-                $selectRequete->execute(array($allumer));
-                //$selectRequete->execute(array($allumer,$numSalle));
-                echo 'allumé<br>';
+            $selectRequete = parent::$db->prepare("SELECT * FROM reservation WHERE userid=? AND TIMEDIFF(heure,NOW())>'-01:00:00' and TIMEDIFF(heure,NOW())<'00:00:00';");
+            $selectRequete->execute(array($userid));
+            $rowcount=$selectRequete->rowCount();
+            if($rowcount=1){
+                $numSalle=getNumSalle();
+                if($allumer == 1){
+                    //$selectRequete = parent::$db->prepare("UPDATE equipement SET etat=?;");
+                    $selectRequete = parent::$db->prepare("UPDATE equipement SET etat=? WHERE numerosalle=?;");
+                    //$selectRequete->execute(array($allumer));
+                    $selectRequete->execute(array($allumer,$numSalle));
+                    echo 'allumé<br>';
+                }
+                else{
+                    //$selectRequete = parent::$db->prepare("UPDATE equipement SET etat=?;");
+                    $selectRequete = parent::$db->prepare("UPDATE equipement SET etat=? WHERE numerosalle=?;");
+                    //$selectRequete->execute(array($allumer));
+                    $selectRequete->execute(array($allumer,$numSalle));
+                    echo 'éteint<br>';
+                }
             }
             else{
-                $selectRequete = parent::$db->prepare("UPDATE equipement SET etat=?;");
-                //$selectRequete = parent::$db->prepare("UPDATE equipement SET etat=? WHERE numerosalle=?;");
-                $selectRequete->execute(array($allumer));
-                //$selectRequete->execute(array($allumer,$numSalle));
-                echo 'éteint<br>';
+                echo 'erreur, vous avez pas de réservation';
             }
         }
 
-        /*public function getEtat(){
-            $selectRequete = parent::$db->prepare("SELECT etat FROM equipement WHERE numerosalle=?;");
-            $selectRequete->execute(array("B112"));
-            $result=$selectRequete->fechAll(PDO::FETCH_ASSOC);
-            echo $result;
-        }*/
-
-        //public function setLuminosite($lum,$numSalle){
         public function setLuminosite($lum){
-            $selectRequete = parent::$db->prepare("UPDATE caracteristique SET valeur=? WHERE idcaracteristique=?;");
-            //$selectRequete = parent::$db->prepare("UPDATE caracteristique SET valeur=? WHERE idcaracteristique=? AND numerosalle=?;");
-            $selectRequete->execute(array($lum,"1"));
-            //$selectRequete->execute(array($lum,"1",$numSalle));
+            $selectRequete = parent::$db->prepare("SELECT * FROM reservation WHERE userid=? AND TIMEDIFF(heure,NOW())>'-01:00:00' and TIMEDIFF(heure,NOW())<'00:00:00';");
+            $selectRequete->execute(array($userid));
+            $rowcount=$selectRequete->rowCount();
+            if($rowcount=1){
+                $numSalle=getNumSalle();
+                //$selectRequete = parent::$db->prepare("UPDATE caracteristique SET valeur=? WHERE idcaracteristique=?;");
+                $selectRequete = parent::$db->prepare("UPDATE caracteristique SET valeur=? WHERE idcaracteristique=? AND numerosalle=?;");
+                //$selectRequete->execute(array($lum,"1"));
+                $selectRequete->execute(array($lum,"1",$numSalle));
+            }
+            else{
+                echo 'erreur, vous avez pas de réservation';
+            }
+            
         }
 
         public function getNumSalle(){
-            $selectRequete=parent::$db->prepare("SELECT numerosalle FROM reservation where userid=?;");
+            $selectRequete=parent::$db->prepare("SELECT numerosalle FROM reservation where userid=? AND TIMEDIFF(heure,NOW())>'-01:00:00' and TIMEDIFF(heure,NOW())<'00:00:00';");
             $selectRequete->execute(array($_SESSION[userid]));
-            $result=$selectRequete->fechAll(PDO::FETCH_ASSOC);
+            $result=$selectRequete->fechAll(PDO::FETCH_ASSOC)[numerosalle];
             return $result;
         }
     }
